fdd142ecc3447d7b13e306654cb18564b04ebba6
Lyndi Castrejon <lyndic@protonmail.com>
2022-10-07 11:38:48 -0600

    Initial Commit

---
+++ chat-app/chat_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  chat_appApp.swift
+//  chat-app
+//
+//  Created by Lyndi Castrejon on 10/7/22.
+//
+
+import SwiftUI
+
+@main
+struct chat_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
